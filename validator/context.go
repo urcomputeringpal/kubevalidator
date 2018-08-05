709fca18b819b90309a65df7710aaf6cd41a850a
Jesse Newland <jesse@jnewland.com>
2018-08-05 12:34:51 -0500

    owner

--- validator/context.go
+++ validator/context.go
@@ -26,7 +26,7 @@ func (c *Context) Process() {
 func (c *Context) ProcessCheckSuite(e *github.CheckSuiteEvent) {
 	if *e.Action == "requested" || *e.Action == "re-requested" {
 		// Determine which files to load
-		fileContent, _, _, err := c.github.Repositories.GetContents(*c.ctx, e.Sender.GetLogin(), e.Repo.GetName(), "skaffold.yaml", &github.RepositoryContentGetOptions{
+		fileContent, _, _, err := c.github.Repositories.GetContents(*c.ctx, e.Repo.GetOwner().GetLogin(), e.Repo.GetName(), "skaffold.yaml", &github.RepositoryContentGetOptions{
 			Ref: e.CheckSuite.GetHeadSHA(),
 		})
 		if err != nil {
